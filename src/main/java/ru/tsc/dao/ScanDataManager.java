b9348d8ac4986ffe9a8dbb5902804a4851439bb0
Pavel <dym91@list.ru>
2017-10-23 19:50:16 +0300

    scan-3

--- src/main/java/ru/tsc/dao/ScanDataManager.java
+++ src/main/java/ru/tsc/dao/ScanDataManager.java
@@ -3,15 +3,85 @@ package ru.tsc.dao;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.tsc.model.Scan;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.sql.rowset.serial.SerialBlob;
+
+
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.sql.*;
 
 @Repository
 @DependsOn(value = "jpaTransactionManager")
 public class ScanDataManager {
+   /* public static void main(String[] args) {
+
+        Connection connection = null;
+        String url = "jdbc:oracle:thin:@ 89.108.84.144:1521/BPM8";
+        String name = "test01";
+        String password = "test01";
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            System.out.println("Драйвер подключен");
+            connection = DriverManager.getConnection(url, name, password);
+            System.out.println("Соединение установлено");
+            Statement statement;
+
+            statement = connection.createStatement();
+
+//            ResultSet rset = statement.executeQuery("SELECT * FROM test01.WR_SCAN");
+//            while (rset.next ()) {
+                File file = new File("scan.jpg");
+                byte[] bytes = new byte[(int) file.length()]; //file.getBytes();
+                try {
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    fileInputStream.read(bytes);
+                    fileInputStream.close();
+                } catch (Exception e) {}
+
+                Blob blob = new SerialBlob(bytes);
+                Scan scan = new Scan("name", "format", blob);
+            System.out.println(bytes);
+            System.out.println(blob);
+            System.out.println(scan);
+//                CHAR id = (CHAR) ((OracleResultSet)rset).getOracleObject (1);
+//                CHAR department = (CHAR) ((OracleResultSet)rset).getOracleObject (2);
+//                System.out.println(id + " " + department);
+//            }
+            ScanDataManager scanDataManager = new ScanDataManager();
+            scanDataManager.addScan(scan);
+
+        } catch (Exception ex) { ex.printStackTrace();
+        } finally { if (connection != null) {try {connection.close();} catch (SQLException ex) {} } }
+
+    }*/
+
+/*    public static void main(String[] args) {
+
+        public void addScan(@RequestParam("scanfile") MultipartFile scanfile){
+//    public void addScan(@RequestBody Scan scan, @RequestParam("scanfile") MultipartFile scanfile){
+            if (!scanfile.isEmpty()) {
+                try {
+                    byte[] bytes = scanfile.getBytes();
+//                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+//                stream.write(bytes);
+//                stream.close();
+
+                    Scan scan = new Scan("fileName", "fileFormat", new SerialBlob(bytes));
+                    scanDataManager.addScan(scan);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }*/
+
 
     @PersistenceContext(unitName = "tscPersistentUnit")
     private EntityManager entityManager;
--- src/main/java/ru/tsc/rest/ScanService.java
+++ src/main/java/ru/tsc/rest/ScanService.java
@@ -2,6 +2,10 @@ package ru.tsc.rest;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.tsc.dao.ScanDataManager;
@@ -11,6 +15,10 @@ import javax.sql.rowset.serial.SerialBlob;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Blob;
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping(value = "/scan")
@@ -19,23 +27,88 @@ public class ScanService {
     @Autowired
     ScanDataManager scanDataManager;
 
-    @RequestMapping(value = "/scan/{id}", method = RequestMethod.GET)
-    public Scan getScan(@PathVariable Long id) { // а тут формат возвращаемого значания должен быть byte[] ?
-        return scanDataManager.getScan(id);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getScan(@PathVariable Long id) throws SQLException {
+        Scan scan = scanDataManager.getScan(id);
+        String format = scan.getFormat();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.valueOf(format)); //MediaType.APPLICATION_OCTET_STREAM
+//        httpHeaders.setContentLength();
+
+        Blob blob = scan.getScanfile();
+//        Path path = Path.get()
+        byte[] bytes = blob.getBytes(1, (int) blob.length());//Files.readAllBytes(path);
+
+        return new ResponseEntity<byte[]>(bytes, httpHeaders, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-//    void addScan(@RequestBody Scan scan) {
-//        scanDataManager.addScan(scan);
-//    }
-    public void addScan(@RequestParam("scanfile") MultipartFile scanfile){
+
+    @RequestMapping(value = "/uploadDocumentFile", method = RequestMethod.POST, consumes = "application/octet-stream", produces = "application/json;charset=utf-8")
+    public void addScan(@RequestBody byte[] bytes, @RequestParam String fileName) throws SQLException{
 //    public void addScan(@RequestBody Scan scan, @RequestParam("scanfile") MultipartFile scanfile){
-        if (!scanfile.isEmpty()) {
-            try {
-                byte[] bytes = scanfile.getBytes();
+        Blob blob = new SerialBlob(bytes);
+
+        Scan scan = new Scan(fileName, "format", blob);
+
+        scanDataManager.addScan(scan);
+
+//        String name = "scan";
+//        if (!scanfile.isEmpty()) {
+//            try {
+//                byte[] bytes = scanfile.getBytes();
 //                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
 //                stream.write(bytes);
 //                stream.close();
+//
+//                Scan scan = new Scan("fileName", "fileFormat", new SerialBlob(bytes));
+//                scanDataManager.addScan(scan);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+    }
+
+    //-------------------------------------------------------------------------------------------
+
+/*
+    @RequestMapping(value = "/{documentId}", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<byte[]> getDocumentFile(@PathVariable String documentId) {
+        byte[] resultBody = new byte[]{}; //получаем файл из базы
+        ResponseEntity<byte[]> result = new ResponseEntity<>(resultBody, HttpStatus.OK);
+        return result;
+    }
+*/
+
+/*
+    @RequestMapping(value = "/uploadDocumentFile", method = RequestMethod.POST, consumes = "application/octet-stream", produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String uploadDocumentFile(@RequestBody byte[] bytes, @RequestParam String fileName) {
+        if (bytes.length != 0) {
+            // сохраняем файл
+            return "fileId";
+        } else {
+            System.out.println("File not found");
+            return null;
+        }
+    }
+*/
+
+    //-------------------------------------------------------------------------------------------
+
+
+
+/*    @RequestMapping(method = RequestMethod.POST)
+    public void addScan(@RequestParam("scanfile") MultipartFile scanfile){
+//    public void addScan(@RequestBody Scan scan, @RequestParam("scanfile") MultipartFile scanfile){
+        String name = "scan";
+        if (!scanfile.isEmpty()) {
+            try {
+                byte[] bytes = scanfile.getBytes();
+                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
+                stream.write(bytes);
+                stream.close();
 
                 Scan scan = new Scan("fileName", "fileFormat", new SerialBlob(bytes));
                 scanDataManager.addScan(scan);
@@ -44,7 +117,7 @@ public class ScanService {
             }
         }
 
-    }
+    }*/
 
     @RequestMapping(value = "/scan", method = RequestMethod.PUT)
     public void updateScan(@RequestBody Scan scan) {
